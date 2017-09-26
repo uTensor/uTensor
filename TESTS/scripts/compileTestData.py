fe835eac4403550acafbf2ea3e4fc40a332a1272
Neil Tan <neil.c.tan@gmail.com>
2017-09-26 19:17:39 +0800

    reformatted the TESTS/script folder; accounted for PRE-GEN data

--- TESTS/scripts/compileTestData.py
+++ TESTS/scripts/compileTestData.py
@@ -1,5 +1,16 @@
 import os
 from pathlib import Path
+import shutil 
+
+DATA_DIR = "PRE-GEN"
+OUTPUT_DIR = "testData"
+
+FULL_OUTPUT_DIR = (Path.cwd() / OUTPUT_DIR).resolve()
+FULL_PREGEN_DIR = (Path.cwd() / DATA_DIR).resolve()
+TEST_DATA_DIR = FULL_OUTPUT_DIR
+
+print("FULL_OUTPUT_DIR", FULL_OUTPUT_DIR)
+print("FULL_PREGEN_DIR", FULL_PREGEN_DIR)
 
 
 def mkdir(path, dirname):
@@ -20,8 +31,17 @@ def mkdir(path, dirname):
     return outPath
 
 
-TEST_DATA_DIR = mkdir(Path.cwd(), "testData")
-run_items = Path.cwd().glob("*/*.py")
+
+
+#remove the output directry and start clean:
+if Path(FULL_OUTPUT_DIR).exists():
+    shutil.rmtree(FULL_OUTPUT_DIR)
+
+#copy the pre-generated date to output:
+shutil.copytree(FULL_PREGEN_DIR, FULL_OUTPUT_DIR)
+
+#run all scripts to generate the data
+run_items = Path.cwd().glob("GEN_SCR/*/*.py")
 
 for item in run_items:
     exec(open(item).read())
