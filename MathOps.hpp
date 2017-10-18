6f2d519eb0450fd241e6bfeb9ea15cd0bb1020d9
Neil Tan <neil.c.tan@gmail.com>
2017-10-19 03:19:03 +0800

    Argmax seems to pass using permuteIndexTransform

--- MathOps.hpp
+++ MathOps.hpp
@@ -115,43 +115,57 @@ void Max(Tensor<TIn> input, Tensor<Td> dim, Tensor<TOut> out) {
 
 template<class TIn, class TOut>
 void ArgMax(Tensor<TIn> input, Tensor<int> dim, Tensor<TOut> &out) {
+
   int dim_reduce = *(dim.getPointer({0}));
-  vector<uint32_t> outShape = input.getShape();
+  Shape outShape = input.getShape();
+  uint32_t reduce_dim_size = outShape[dim_reduce];
   outShape.erase(outShape.begin()+dim_reduce);
 
+  //construct the permute vector
+  vector<uint8_t> permute;
+  for(uint8_t i = 0; i < input.getShape().size(); i++) {
+    permute.push_back(i);
+  }
+  permute.push_back(static_cast<uint8_t>(dim_reduce));
+  permute.erase(permute.begin()+dim_reduce);
+  
+  //check dimensionality
   if(out.getSize() != 0 && out.getShape() != outShape) {
     ERR_EXIT("output shape mismatch");
   }
 
+  //allocate output tensor if empty
   if(out.getSize() == 0) {
     out = Tensor<TOut>(outShape);
   }
 
+  //construct the origin-shape for permuteIndexTransform
+  Shape vOutShape = outShape;
+  vOutShape.push_back(reduce_dim_size);
+  ///NT: easy way to remember...
+  //trans(originShape, permute)
+  //targetIndex = trans[OriginIndex]
+  //In this case, we are going backward.
+  permuteIndexTransform trans(vOutShape, permute);
+
   TIn* inPtr = input.getPointer({});
   TOut* outPtr = out.getPointer({});
 
-  size_t d_size = input.getShape()[dim_reduce];
-  size_t rem_d_size = out.getSize();
-  uint32_t d_stride = input.getStride(dim_reduce);
+  size_t out_index = 0;
 
-  for(size_t i = 0; i < rem_d_size; i++) {
-    TOut max = std::numeric_limits<TOut>::lowest();
-    for(size_t d = 0; d < d_size; d++) {
-      TIn tmp;
-
-      ///NT: probably won't work for dim > 1
-      if(d_stride == 1) {
-        tmp = inPtr[d + i * d_size];
-      } else {
-        tmp = inPtr[d * d_stride + i];
-      }
-
-      //TIn tmp = inPtr[d * d_stride + i * d_size];
-      if(tmp > max) {
-        outPtr[i] = d;
-        max = tmp;
+  for(uint32_t i = 0; i < input.getSize(); i+=reduce_dim_size) {
+    TOut max_j = 0;
+    TIn last_max = std::numeric_limits<TIn>::min();
+    for(uint32_t j = 0; j < reduce_dim_size; j++) {
+      TIn val = inPtr[trans[i + j]];
+      if(val > last_max) {
+        last_max = val;
+        max_j = j;
       }
     }
+
+    outPtr[out_index] = max_j;
+    out_index++;
   }
 }
 #endif  // UTENSOR_MATH_OPS
