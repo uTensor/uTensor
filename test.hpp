2f823603f4c9047e97a9242e64bca17d134b1072
kazami <kazamizero@gmail.com>
2017-10-19 12:47:19 +0800

    Add test function and refactor code to correct style
      1. add function to testval and testshape for transform patch
      2. follow the coding to google style
      3. include the vector file to make the program portable

--- test.hpp
+++ test.hpp
@@ -1,188 +1,183 @@
 #ifndef UTENSOR_TEST
 #define UTENSOR_TEST
 
-#include <string>
 #include <math.h>
 #include <limits>
+#include <string>
+#include <vector>
 
 class Test {
-    
-        protected:
-            string testName;
-            string summary;
+ protected:
+  string testName;
+  string summary;
 
-            void printStatus(string status) {
-                int pLen = std::max(1, 30 - (int) testName.length());
-                string msg = testName;
+  void printStatus(string status) {
+    int pLen = std::max(1, 30 - (int)testName.length());
+    string msg = testName;
 
-                for(int i = 0; i < pLen; i++) {
-                    msg += " ";
-                }
+    for (int i = 0; i < pLen; i++) {
+      msg += " ";
+    }
 
-                msg += "[ " + status + " ]";
-                summary += msg + "\r\n";
+    msg += "[ " + status + " ]";
+    summary += msg + "\r\n";
 
-                if(print_test) printf("%s\r\n", msg.c_str());
-            }
+    if (print_test) printf("%s\r\n", msg.c_str());
+  }
 
-            void testStart(string _testName) {
-                testName = _testName;
-                numTotal++;
-            }
+  void testStart(string _testName) {
+    testName = _testName;
+    numTotal++;
+  }
 
-            void passed(bool res = true) {
-                if(!res) {
-                    failed();
-                    return;
-                }
+  void passed(bool res = true) {
+    if (!res) {
+      failed();
+      return;
+    }
 
-                if(testName == "") {
-                    printf("Error: test name not cleared piror to test run\r\n");
-                    exit(1);
-                }
+    if (testName == "") {
+      printf("Error: test name not cleared piror to test run\r\n");
+      exit(1);
+    }
 
-                numOk++;
-                printStatus("  OK  ");
+    numOk++;
+    printStatus("  OK  ");
 
-                testName = "";
-            }
+    testName = "";
+  }
 
-            void failed() {
-                if(testName == "") {
-                    printf("Error: testStart is not called prior to test start\r\n");
-                    exit(1);
-                }
+  void failed() {
+    if (testName == "") {
+      printf("Error: testStart is not called prior to test start\r\n");
+      exit(1);
+    }
 
-                numFailed++;
-                printStatus("** FAILED **");
+    numFailed++;
+    printStatus("** FAILED **");
 
-                testName = "";
-            }
+    testName = "";
+  }
 
-            void warn() {
-                if(testName == "") {
-                    printf("Error: testStart is not called prior to test start\r\n");
-                    exit(1);
-                }
+  void warn() {
+    if (testName == "") {
+      printf("Error: testStart is not called prior to test start\r\n");
+      exit(1);
+    }
 
-                numWarn++;
-                printStatus(" * WARN * ");
+    numWarn++;
+    printStatus(" * WARN * ");
 
-                testName = "";
-            }
-    
-        public:
-            unsigned int numOk;
-            unsigned int numFailed;
-            unsigned int numWarn;
-            unsigned int numTotal;
-            bool print_test;
-    
-            Test() {
-                numOk = 0;
-                numFailed = 0;
-                numWarn = 0;
-                testName = "";
-                summary = "";
-                print_test = false;
-            }
+    testName = "";
+  }
 
-            void printSummary(void) {
-                printf("%s\r\n", summary.c_str());
-            }
+ public:
+  unsigned int numOk;
+  unsigned int numFailed;
+  unsigned int numWarn;
+  unsigned int numTotal;
+  bool print_test;
 
-            virtual void runAll(void) = 0;
+  Test() {
+    numOk = 0;
+    numFailed = 0;
+    numWarn = 0;
+    testName = "";
+    summary = "";
+    print_test = false;
+  }
 
-            template<typename U>
-            double sum(Tensor<U> input) {
-                U* elem = input.getPointer({});
-                double accm = 0.0;
-                for(uint32_t i = 0; i < input.getSize(); i++) {
-                    accm += (double) elem[i];
-                }
-            
-                return accm;
-            }
-            template<typename T>
-            bool testshape(vector<T> src, vector<T> res, vector<uint8_t> permute) {
-              bool pass = true;
-              for (size_t i = 0; i < permute.size(); i++) {
-                  if (src[permute[i]] != res[i]) {
-                      pass = false;
-                      return pass;
-                  }
-                      
-              }
-              return pass;
-            }
-            template<typename T>
-            bool testval(T src, T res) {
-              bool pass = true;
-              if (src == res) {
-                  return pass;
-              }
-              return false;
-            }
+  void printSummary(void) { printf("%s\r\n", summary.c_str()); }
 
-            template<typename U>
-            double meanAbsErr(Tensor<U> A, Tensor<U> B) {
-                if(A.getSize() != B.getSize()) {
-                    DEBUG("Test.meanAbsErr(): dimension mismatch\r\n");
-                }
+  virtual void runAll(void) = 0;
 
-                U* elemA = A.getPointer({});
-                U* elemB = B.getPointer({});
+  template <typename U>
+  double sum(Tensor<U> input) {
+    U* elem = input.getPointer({});
+    double accm = 0.0;
+    for (uint32_t i = 0; i < input.getSize(); i++) {
+      accm += (double)elem[i];
+    }
 
-                double accm = 0.0;
-                for(uint32_t i = 0; i < A.getSize(); i++) {
-                    accm += (double) fabs((float) elemB[i] - (float) elemA[i]);
-                }
-            
-                return accm;
-            }
+    return accm;
+  }
+  template <typename T>
+  bool testshape(std::vector<T> src, std::vector<T> res,
+                 std::vector<uint8_t> permute) {
+    bool pass = true;
+    for (size_t i = 0; i < permute.size(); i++) {
+      if (src[permute[i]] != res[i]) {
+        pass = false;
+        return pass;
+      }
+    }
+    return pass;
+  }
+  template <typename T>
+  bool testval(T src, T res) {
+    bool pass = true;
+    if (src == res) {
+      return pass;
+    }
+    return false;
+  }
 
-            //A being the reference
-            template<typename U>
-            double meanPercentErr(Tensor<U> A, Tensor<U> B) {
-                if(A.getSize() != B.getSize()) {
-                    DEBUG("Test.meanAbsErr(): dimension mismatch\r\n");
-                }
+  template <typename U>
+  double meanAbsErr(Tensor<U> A, Tensor<U> B) {
+    if (A.getSize() != B.getSize()) {
+      DEBUG("Test.meanAbsErr(): dimension mismatch\r\n");
+    }
 
-                U* elemA = A.getPointer({});
-                U* elemB = B.getPointer({});
+    U* elemA = A.getPointer({});
+    U* elemB = B.getPointer({});
 
-                double accm = 0.0;
-                for(uint32_t i = 0; i < A.getSize(); i++) {
-                    if(elemA[i] != 0.0f) {
-                        accm += (double) fabs(((float) elemB[i] - (float) elemA[i]) / fabs((float) elemA[i]));
-                    } else {
-                        if(elemB[i] != 0) {
-                            accm += std::numeric_limits<float>::quiet_NaN();
-                        }
-                    }
-                }
-            
-                return accm;
-            }
+    double accm = 0.0;
+    for (uint32_t i = 0; i < A.getSize(); i++) {
+      accm += (double)fabs((float)elemB[i] - (float)elemA[i]);
+    }
 
+    return accm;
+  }
+
+  // A being the reference
+  template <typename U>
+  double meanPercentErr(Tensor<U> A, Tensor<U> B) {
+    if (A.getSize() != B.getSize()) {
+      DEBUG("Test.meanAbsErr(): dimension mismatch\r\n");
+    }
+
+    U* elemA = A.getPointer({});
+    U* elemB = B.getPointer({});
+
+    double accm = 0.0;
+    for (uint32_t i = 0; i < A.getSize(); i++) {
+      if (elemA[i] != 0.0f) {
+        accm += (double)fabs(((float)elemB[i] - (float)elemA[i]) /
+                             fabs((float)elemA[i]));
+      } else {
+        if (elemB[i] != 0) {
+          accm += std::numeric_limits<float>::quiet_NaN();
+        }
+      }
+    }
+
+    return accm;
+  }
 };
 
-//https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
-void printBits(size_t const size, void const * const ptr)
-{
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
+// https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
+void printBits(size_t const size, void const* const ptr) {
+  unsigned char* b = (unsigned char*)ptr;
+  unsigned char byte;
+  int i, j;
 
-    for (i=size-1;i>=0;i--)
-    {
-        for (j=7;j>=0;j--)
-        {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
+  for (i = size - 1; i >= 0; i--) {
+    for (j = 7; j >= 0; j--) {
+      byte = (b[i] >> j) & 1;
+      printf("%u", byte);
     }
-    puts("");
+  }
+  puts("");
 }
 
 #endif
--- uTensor_util.hpp
+++ uTensor_util.hpp
@@ -1,106 +1,109 @@
 #ifndef UTENSOR_UTIL
 #define UTENSOR_UTIL
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
 
 // #define MAX(A, B) ((A > B)? A:B)
 
 #if MBED_CONF_APP_DEBUG_MSG
-void return_error(int ret_val){
-    if (ret_val) {
-      printf(" [**Failure**] %d\r\n", ret_val);
-      printf("Exiting...\r\n");
-      fflush(stdout);      
-      exit(-1);
-    } else {
-      printf("  [DONE]\r\n");
-    }
+void return_error(int ret_val) {
+  if (ret_val) {
+    printf(" [**Failure**] %d\r\n", ret_val);
+    printf("Exiting...\r\n");
+    fflush(stdout);
+    exit(-1);
+  } else {
+    printf("  [DONE]\r\n");
+  }
 }
 
-void errno_error(void* ret_val){
-    if (ret_val == NULL) {
-      printf(" [**Failure**] %d \r\n", errno);
-      printf("Exiting...\r\n");
-      fflush(stdout);
-      exit(-1);
-    } else {
-      printf("  [DONE]\r\n");
-    }
+void errno_error(void* ret_val) {
+  if (ret_val == NULL) {
+    printf(" [**Failure**] %d \r\n", errno);
+    printf("Exiting...\r\n");
+    fflush(stdout);
+    exit(-1);
+  } else {
+    printf("  [DONE]\r\n");
+  }
 }
 
+#define ON_ERR(FUNC, MSG) \
+  {                       \
+    printf(" * ");        \
+    printf(MSG);          \
+    return_error(FUNC);   \
+  }
 
-#define ON_ERR(FUNC, MSG)   {                       \
-                                printf(" * ");      \
-                                printf(MSG);        \
-                                return_error(FUNC); \
-                            }
+#define DEBUG(MSG, ...)         \
+  {                             \
+    printf(MSG, ##__VA_ARGS__); \
+    fflush(stdout);             \
+  }
 
-#define DEBUG(MSG, ...)     {                               \
-                                printf(MSG, ##__VA_ARGS__); \
-                                fflush(stdout);             \
-                            }
+#else  // MBED_CONF_APP_DEBUG_MSG
 
-#else //MBED_CONF_APP_DEBUG_MSG
-
-void errno_error(void* ret_val) { /*DOES NOTHING*/}
+void errno_error(void* ret_val) { /*DOES NOTHING*/
+}
 #define ON_ERR(FUNC, MSG) FUNC
 #define DEBUG(MSG, ...)
 
 #endif
 
-#define ERR_EXIT(MSG, ...)  {                                       \
-                                printf("[Error] %s:%d @%s ", __FILE__, __LINE__, __func__); \
-                                printf(MSG, ##__VA_ARGS__);         \
-                                fflush(stdout);                     \
-                                exit(-1);                           \
-                            }
-
+#define ERR_EXIT(MSG, ...)                                      \
+  {                                                             \
+    printf("[Error] %s:%d @%s ", __FILE__, __LINE__, __func__); \
+    printf(MSG, ##__VA_ARGS__);                                 \
+    fflush(stdout);                                             \
+    exit(-1);                                                   \
+  }
 
 typedef long long int64;
-typedef vector<uint32_t> Shape;
+typedef std::vector<uint32_t> Shape;
 
-void printVector(vector<uint32_t> vec) {
-    printf("vector: \r\n");
-    for(uint32_t i:vec) {
-        printf("%d ", (unsigned int) i);
-    }
+void printVector(std::vector<uint32_t> vec) {
+  printf("vector: \r\n");
+  for (uint32_t i : vec) {
+    printf("%d ", (unsigned int)i);
+  }
 
-    printf("\r\n");
-
+  printf("\r\n");
 }
 
+// little endian to big endian
+uint32_t htonl(uint32_t& val) {
+  const uint32_t mask = 0b11111111;
+  uint32_t ret = 0;
 
-//little endian to big endian
-uint32_t htonl(uint32_t &val) {
-    const uint32_t mask = 0b11111111;
-    uint32_t ret = 0;
-    
-    ret |= val >> 24;
-    ret |= (val & (mask << 16)) >> 8;
-    ret |= (val & (mask << 8)) << 8;
-    ret |= val << 24;
+  ret |= val >> 24;
+  ret |= (val & (mask << 16)) >> 8;
+  ret |= (val & (mask << 8)) << 8;
+  ret |= val << 24;
 
-    return ret;
+  return ret;
 }
 
-//big endian to little endian
+// big endian to little endian
 uint16_t ntoh16(uint16_t val) {
-    uint16_t ret = 0;
-    
-    ret |= val >> 8;
-    ret |= val << 8;
+  uint16_t ret = 0;
 
-    return ret;
+  ret |= val >> 8;
+  ret |= val << 8;
+
+  return ret;
 }
 
 uint32_t ntoh32(uint32_t val) {
-    const uint32_t mask = 0b11111111;
-    uint32_t ret = 0;
-    
-    ret |= val >> 24;
-    ret |= (val & (mask << 16)) >> 8;
-    ret |= (val & (mask << 8)) << 8;
-    ret |= val << 24;
+  const uint32_t mask = 0b11111111;
+  uint32_t ret = 0;
 
-    return ret;
+  ret |= val >> 24;
+  ret |= (val & (mask << 16)) >> 8;
+  ret |= (val & (mask << 8)) << 8;
+  ret |= val << 24;
+
+  return ret;
 }
 
 #endif
