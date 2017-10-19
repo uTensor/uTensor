6861f375d70c1c4ccf50b937c6dd032dc2df952e
kazami <kazamizero@gmail.com>
2017-10-19 12:42:21 +0800

    Add test for tensor transform   1. add test for shape correctness   2. add test for 3d && 4d array to make sure the transform is correct   3. add test for odd and even number elements in the array is correct   4. refactor the code to make clean   5. fix the bug, which return reference of stack variable in operator[]   6. the test reference value is made from numpy and numpy transpose

--- tensor.hpp
+++ tensor.hpp
@@ -1,265 +1,243 @@
 #ifndef UTENSOR_TENSOR_H
 #define UTENSOR_TENSOR_H
 
-#include "mbed.h"
-#include <memory>
-#include <vector>
 #include <initializer_list>
-#include "stdlib.h"
+#include <memory>
 #include <uTensor_util.hpp>
+#include <vector>
+#include "mbed.h"
+#include "stdlib.h"
 
 template <class U>
 class TensorBase {
-public:
-    vector<uint32_t> shape;
-    U* data;
-    uint32_t total_size;
+ public:
+  vector<uint32_t> shape;
+  U* data;
+  uint32_t total_size;
 
-    ~TensorBase() {
-        DEBUG("TensorBase destruction..\r\n");
-        free(data);
-    }
+  ~TensorBase() {
+    DEBUG("TensorBase destruction..\r\n");
+    free(data);
+  }
 };
 
 template <class T>
 class Tensor {
-    std::shared_ptr<TensorBase<T>> s; //short for states
+  std::shared_ptr<TensorBase<T>> s;  // short for states
 
-    void init(vector<uint32_t> &v) { s = std::make_shared<TensorBase<T>>(TensorBase<T>());
-        s->total_size = 0;
-        
-        for(auto i:v) {
-            s->shape.push_back(i);
-            //total_size = (total_size == 0)? i : total_size *= i;
-            if(s->total_size == 0) {
-                s->total_size = i;
-            } else {
-                s->total_size *= i;
-            }
-        }
+  void init(vector<uint32_t>& v) {
+    s = std::make_shared<TensorBase<T>>(TensorBase<T>());
+    s->total_size = 0;
 
-        
-        s->data = (T*) malloc(unit_size() * s->total_size);
-        //printf("total size is:%d\r\n", (int) total_size);
+    for (auto i : v) {
+      s->shape.push_back(i);
+      // total_size = (total_size == 0)? i : total_size *= i;
+      if (s->total_size == 0) {
+        s->total_size = i;
+      } else {
+        s->total_size *= i;
+      }
     }
 
+    s->data = (T*)malloc(unit_size() * s->total_size);
+    // printf("total size is:%d\r\n", (int) total_size);
+  }
 
-    public:
+ public:
+  Tensor(void) { s->total_size = 0; }
 
-    Tensor(void) {
-        s->total_size = 0;
+  Tensor(initializer_list<uint32_t> l) {
+    vector<uint32_t> v;
+    for (auto i : l) {
+      v.push_back(i);
     }
 
-    Tensor(initializer_list<uint32_t> l) {
-        vector<uint32_t> v;
-        for(auto i:l) {
-            v.push_back(i);
-        }
+    init(v);
+  }
 
-        init(v);
+  Tensor(vector<uint32_t> v) { init(v); }
+
+  // returns how far a given dimension is apart
+  size_t getStride(size_t dim_index) {
+    unsigned int size_accm = 1;
+    for (auto it = s->shape.begin() + dim_index + 1; it != s->shape.end();
+         it++) {
+      size_accm *= *it;
     }
 
-    Tensor(vector<uint32_t> v) {
-        init(v);
+    return (size_t)size_accm;
+  }
+
+  // PRE:      l, initization list, specifying the element/dimension
+  // POST:     When a degenerative index is supplied, the pointer
+  //          lowest specified dimension is returned.
+  //          Otherwise, return the pointer to the specific element.
+  T* getPointer(initializer_list<size_t> l) {
+    size_t p_offset = 0;
+    signed short current_dim = 0;
+    for (auto i : l) {
+      p_offset += i * getStride(current_dim);
+      current_dim++;
     }
 
-    //returns how far a given dimension is apart
-    size_t getStride(size_t dim_index) {
-        unsigned int size_accm = 1;
-        for(auto it = s->shape.begin() + dim_index + 1; it != s->shape.end(); it++) {
-            size_accm *= *it;
-        }
+    // printf("p_offset: %d\r\n", p_offset);
+    return s->data + p_offset;
+  }
 
-        return (size_t) size_accm;
+  T* getPointer(vector<uint32_t> v) {
+    size_t p_offset = 0;
+    signed short current_dim = 0;
+    for (auto i : v) {
+      p_offset += i * getStride(current_dim);
+      current_dim++;
     }
 
-    //PRE:      l, initization list, specifying the element/dimension
-    //POST:     When a degenerative index is supplied, the pointer
-    //          lowest specified dimension is returned.
-    //          Otherwise, return the pointer to the specific element.
-    T* getPointer(initializer_list<size_t> l) {
-        size_t p_offset = 0;
-        signed short current_dim = 0;
-        for(auto i:l) {
-            p_offset += i * getStride(current_dim);
-            current_dim++;
-        }
+    printf("p_offset: %d\r\n", p_offset);
 
-        //printf("p_offset: %d\r\n", p_offset);
-        return s->data + p_offset;
-    }
+    return s->data + p_offset;
+  }
 
-    T* getPointer(vector<uint32_t> v) {
-        size_t p_offset = 0;
-        signed short current_dim = 0;
-        for(auto i:v) {
-            p_offset += i * getStride(current_dim);
-            current_dim++;
-        }
+  vector<uint32_t>& getShape(void) { return s->shape; }
 
-        printf("p_offset: %d\r\n", p_offset);
+  uint32_t getSize(void) { return s->total_size; }
 
-        return s->data + p_offset;
-    }
+  uint16_t unit_size(void) { return sizeof(T); }
 
-    vector<uint32_t>& getShape(void) {
-        return s->shape;
-    }
+  uint32_t getSize_in_bytes(void) { return s->total_size * unit_size(); }
 
-    uint32_t getSize(void) {
-        return s->total_size;
-    }
+  // returns the number of dimensions in the tensor
+  size_t getDim(void) { return s->shape.size(); }
 
-    uint16_t unit_size(void) {
-        return sizeof(T);
-    }
+  ~Tensor() {
+    // if(data != NULL)
+    //     free(data);
 
-    uint32_t getSize_in_bytes(void) {
-        return s->total_size * unit_size();
-    }
-
-    //returns the number of dimensions in the tensor
-    size_t getDim(void) {
-        return s->shape.size();
-    }
-
-    ~Tensor() {
-        // if(data != NULL)
-        //     free(data);
-
-        DEBUG("Tensor Destructed\r\n");
-    }
+    DEBUG("Tensor Destructed\r\n");
+  }
 };
 
-//Usage:
-//Tensor<int> inputTensor({10,10,100,40});
-//vector<uint8_t> permute = {2,3,0,1};
+// Usage:
+// Tensor<int> inputTensor({10,10,100,40});
+// vector<uint8_t> permute = {2,3,0,1};
 //
-//permuteIndexTransform trans(inputTensor.getShape(), permute);
+// permuteIndexTransform trans(inputTensor.getShape(), permute);
 //
-//Tensor<int> outputTensor(trans.getNewShape());  //of shape {100,40,10,10}
-//size_t output_buffer_index = trans[input_buffer_index];
+// Tensor<int> outputTensor(trans.getNewShape());  //of shape {100,40,10,10}
+// size_t output_buffer_index = trans[input_buffer_index];
 
 class permuteIndexTransform {
-private:
-    vector<uint8_t> permute;
-    vector<uint8_t> unpermute;
-    Shape in_shape;
-    Shape in_stride;
-    Shape out_shape;
-    Shape out_stride;
+ private:
+  vector<uint8_t> permute;
+  vector<uint8_t> depermute;
+  Shape in_shape;
+  Shape in_stride;
+  Shape out_shape;
+  Shape out_stride;
 
-    void computeOutputShape(void) {
-        out_stride.clear();
-        if(in_shape.empty()) ERR_EXIT("input shape not set");
-        if(permute.empty() || permute.size() != in_shape.size())
-            ERR_EXIT("invalid permute vector");
+  void computeOutputShape(void) {
+    out_stride.clear();
+    if (in_shape.empty()) ERR_EXIT("input shape not set");
+    if (permute.empty() || permute.size() != in_shape.size())
+      ERR_EXIT("invalid permute vector");
 
-        for(auto&& curr_axis:permute) {
-            out_shape.push_back(in_shape[curr_axis]);
-        }
+    for (auto&& curr_axis : permute) {
+      out_shape.push_back(in_shape[curr_axis]);
+    }
+  }
 
+  size_t evalStride(size_t dim_index, Shape s) {
+    unsigned int size_accm = 1;
+    for (auto it = s.begin() + dim_index + 1; it != s.end(); it++) {
+      size_accm *= *it;
     }
 
-    size_t evalStride(size_t dim_index, Shape s) {
-        unsigned int size_accm = 1;
-        for(auto it = s.begin() + dim_index + 1; it != s.end(); it++) {
-            size_accm *= *it;
-        }
+    return (size_t)size_accm;
+  }
 
-        return (size_t) size_accm;
+  void computeInputStride(void) {
+    in_stride.clear();
+    for (uint32_t i = 0; i < in_shape.size(); i++) {
+      in_stride.push_back(evalStride(i, in_shape));
+    }
+  }
+  void computeOutputStride(void) {
+    out_stride.clear();
+    for (uint32_t i = 0; i < out_shape.size(); i++) {
+      out_stride.push_back(evalStride(i, out_shape));
+    }
+  }
+
+ public:
+  permuteIndexTransform(Shape input_shape, vector<uint8_t> permute) {
+    setInputShape(input_shape);
+    setPermute(permute);
+    apply();
+  }
+
+  vector<uint8_t> getPermute(void) { return permute; }
+  void setPermute(vector<uint8_t>& _permute) {
+    permute = _permute;
+    depermute.resize(permute.size());
+    uint8_t i = 0;
+    for (auto a : permute) {
+      depermute[a] = i;
+      i++;
+    }
+  }
+
+  void setInputShape(Shape s) { in_shape = s; }
+  Shape getNewShape(void) { return out_shape; }
+
+  void apply(void) {
+    computeOutputShape();
+    computeOutputStride();
+    computeInputStride();
+  }
+
+  // size_t forward(size_t index) {
+  //     size_t out_index = 0;
+  //     size_t rem = index;
+
+  //     for(size_t curr_dim = 0; curr_dim < in_shape.size(); curr_dim++) {
+  //         size_t curr_dim_size = in_shape[curr_dim];
+  //         out_index += (rem / curr_dim_size) * out_stride[curr_dim];
+  //         rem = rem % curr_dim_size;
+  //     }
+
+  //     return out_index;
+  // }
+
+  size_t operator[](const size_t index) {
+    size_t out_index = 0;
+    size_t rem = index;
+
+    for (size_t curr_dim = 0; curr_dim < in_shape.size(); curr_dim++) {
+      size_t curr_stride = in_stride[curr_dim];
+      out_index += (rem / curr_stride) * out_stride[depermute[curr_dim]];
+      rem = rem % curr_stride;
     }
 
-/*    void computeOutputStride(void) {
-        out_stride.clear();
-        for(uint32_t i = 0; i < out_shape.size(); i++) {
-            out_stride.push_back(evalStride(i, out_shape));
-        }
-    }*/
-    void computeInputStride(void) {
-        in_stride.clear();
-        for(uint32_t i = 0; i < in_shape.size(); i++) {
-            in_stride.push_back(evalStride(i, in_shape));
-        }
-    }
-    void computeOutputStride(void) {
-        out_stride.clear();
-        for(uint32_t i = 0; i < out_shape.size(); i++) {
-            out_stride.push_back(evalStride(i, out_shape));
-        }
-    }
+    out_index += rem;
 
-public:
-    permuteIndexTransform(Shape input_shape, vector<uint8_t> permute) {
-        setInputShape(input_shape);
-        setPermute(permute);
-        apply();
-    }
+    return out_index;
+  }
 
-    vector<uint8_t> getPermute(void) { return permute; }
-    void setPermute(vector<uint8_t> &_permute) { 
-        permute = _permute; 
-        unpermute.resize(permute.size());
-        uint8_t i = 0;
-        for (auto a : permute) {
-            unpermute[a] = i;
-            i++;
-        }
-    }
+  /*size_t operator[] (const size_t index)
+         {
+             size_t out_index = 0;
+             size_t rem = index;
 
-    void setInputShape(Shape s) { in_shape = s; }
-    Shape getNewShape(void) { return out_shape; }
+             for(size_t curr_dim = 0; curr_dim < in_shape.size(); curr_dim++) {
+                 size_t curr_dim_size = in_shape[curr_dim];
 
-    void apply(void) {
-        computeOutputShape();
-        computeOutputStride();
-        computeInputStride();
-    }
+                 out_index += (rem / curr_dim_size) * out_stride[curr_dim];
+                 rem = rem % curr_dim_size;
+             }
 
-    // size_t forward(size_t index) {
-    //     size_t out_index = 0;
-    //     size_t rem = index;
+             out_index += rem;
 
-    //     for(size_t curr_dim = 0; curr_dim < in_shape.size(); curr_dim++) {
-    //         size_t curr_dim_size = in_shape[curr_dim];
-    //         out_index += (rem / curr_dim_size) * out_stride[curr_dim];
-    //         rem = rem % curr_dim_size;
-    //     }
-
-    //     return out_index;
-    // }
-
-    size_t operator[] (const size_t index)
-    {
-        size_t out_index = 0;
-        size_t rem = index;
-
-        for(size_t curr_dim = 0; curr_dim < in_shape.size(); curr_dim++) {
-            size_t curr_stride = in_stride[curr_dim];
-            out_index += (rem / curr_stride) * out_stride[unpermute[curr_dim]];
-            rem = rem % curr_stride;
-        }
-
-        out_index += rem;
-
-        return out_index;
-    }
-     /*size_t operator[] (const size_t index)
-            {
-                size_t out_index = 0;
-                size_t rem = index;
-
-                for(size_t curr_dim = 0; curr_dim < in_shape.size(); curr_dim++) {
-                    size_t curr_dim_size = in_shape[curr_dim];
-
-                    out_index += (rem / curr_dim_size) * out_stride[curr_dim];
-                    rem = rem % curr_dim_size;
-                }
-
-                out_index += rem;
-
-                return out_index;
-            } */
+             return out_index;
+         } */
 };
 
 #endif
--- tensor_test.hpp
+++ tensor_test.hpp
@@ -1,204 +1,108 @@
 #ifndef UTENSOR_impl_TESTS
 #define UTENSOR_impl_TESTS
 
-#include "test.hpp"
+#include <algorithm>
+#include <random>
 #include "NnOps.hpp"
 #include "tensorIdxImporter.hpp"
-#include <random>
-#include <iostream>
-#include <algorithm>
-
-class read {
-    public:
-        void impl() {
-          for (int i = 0; i < 1000000; i++) {
-            i++;
-          }
-        }
-};
-class A {
-  public:
-      void impl() {
-        for (int i = 0; i < 1000000; i++) {
-          i++;
-        }
-      }
-};
-
-class B {
-    public:
-      virtual void impl() = 0;
-};
-
-class C : public B {
-  public:
-      void impl() override{
-        for (int i = 0; i < 1000000; i++) {
-          i++;
-        }
-      }
-};
-class D {
-    private:
-        read r;
-  public:
-
-      void impl() {
-        r.impl();     
-      }
-};
-template<typename T>
-class vectesthelper {
-    public:
-        vectesthelper(size_t d1 = 0, size_t d2 = 0, size_t d3 = 0, T const & t = T()) :
-            d1(d1), d2(d2), d3(d3), data(d1*d2*d3, t)
-    {}
-
-        T& operator() (size_t i, size_t j, size_t k) {
-            return data[i *d2 * d3 + j * d3 + k];
-        }
-    std::vector<T> data;
-    private:
-        size_t d1, d2, d3;
-};
+#include "test.hpp"
 
 class transTest : public Test {
-  public:
-    void a() {
-        std::random_device rd;
-        std::default_random_engine gen = std::default_random_engine(rd());
+ public:
+  void runShapeTest() {
+    std::random_device rd;
+    std::default_random_engine gen = std::default_random_engine(rd());
+    bool res = false;
 
-        for (int i = 0; i < 10; i++) {
-        testStart("transtest");
-        Tensor<int> inputTensor({10, 10, 100, 40});
-        vector<uint32_t> g = inputTensor.getShape();
-        vector<uint8_t> permute = {2, 3, 0, 1};
-        std::shuffle(permute.begin(), permute.end(), gen);
+    for (int i = 0; i < 10; i++) {
+      testStart("transtest");
+      Tensor<int> inputTensor({10, 10, 100, 40});
+      vector<uint32_t> g = inputTensor.getShape();
+      vector<uint8_t> permute = {2, 3, 0, 1};
+      std::shuffle(permute.begin(), permute.end(), gen);
 
-        permuteIndexTransform trans(inputTensor.getShape(), permute);
+      permuteIndexTransform trans(inputTensor.getShape(), permute);
 
-        Tensor<int> output(trans.getNewShape());
-        vector<uint32_t> s = output.getShape();
-        bool res = testshape<uint32_t>(g, s, permute);
-        passed(res);
-        }
+      Tensor<int> output(trans.getNewShape());
+      vector<uint32_t> s = output.getShape();
+      res = testshape<uint32_t>(g, s, permute);
+      if (!res) {
+        failed();
+      }
     }
-    void b() {
-        vector<int> in_d({2, 5, 4, 5, 2, 6, 5, 1, 3, 6, 7, 9, 1, 2, 3, 4, 3, 5, 6, 9, 2, 3, 3, 2});
-     
-        vector<int> ou_d({2, 2, 3, 5, 6, 6, 4, 5, 7, 5, 1, 9, 1, 3, 2, 2, 5, 3, 3, 6, 3, 4, 9, 2});
+    passed(res);
+  }
+  void runPermuteTest() {
+    // source numpy array dim (2, 3, 4)
+    vector<int> input_1({2, 5, 4, 5, 2, 6, 5, 1, 3, 6, 7, 9,
+                         1, 2, 3, 4, 3, 5, 6, 9, 2, 3, 3, 2});
+    // target numpy array (after np.transpose(0, 2, 1))
+    vector<int> output_1({2, 2, 3, 5, 6, 6, 4, 5, 7, 5, 1, 9,
+                          1, 3, 2, 2, 5, 3, 3, 6, 3, 4, 9, 2});
 
-        Tensor<int> inputTensor({2, 3, 4});
-        vector<uint8_t> permute = {0, 2, 1};
+    Tensor<int> inputTensor({2, 3, 4});
+    vector<uint8_t> permute = {0, 2, 1};
 
-        permuteIndexTransform trans(inputTensor.getShape(), permute);
-        size_t i = 0;
-        size_t o = 0;
-        bool res = false;
+    permuteIndexTransform trans(inputTensor.getShape(), permute);
+    size_t o = 0;
+    bool res = false;
 
-        for (uint32_t i = 0; i < in_d.size(); i++) {
-            testStart("test vec 1 for transform");
-            o = trans[i];
-            res = testval(in_d[i], ou_d[o]);
-            passed(res);
-            res = false;
-        }
-
-
-
-        res = false;
-        vector<int> in_d2({2, 2, 3, 5, 6, 6, 4, 5, 7, 5, 1, 9, 1, 3, 2, 2, 5, 3, 3, 6, 3, 4, 9, 2});
-     
-        vector<int> ou_d2({2, 1, 2, 3, 3, 2, 5, 2, 6, 5, 6, 3, 4, 3, 5, 6, 7, 3, 5, 4, 1, 9, 9, 2});
-
-        Tensor<int> inputTensor2({2, 4, 3});
-        vector<uint8_t> permute2 = {1, 2, 0};
-        permuteIndexTransform trans2(inputTensor2.getShape(), permute2);
-        for (uint32_t i = 0; i < 24; i++) {
-          testStart("test vec 2 for transform");
-          o = trans2[i];
-          res = testval(in_d2[i], ou_d2[o]);
-          passed(res); 
-          res = false;
-        }
-
+    for (uint32_t i = 0; i < input_1.size(); i++) {
+      testStart("test vec 1 for transform");
+      o = trans[i];
+      res = testval(input_1[i], output_1[o]);
+      if (!res) {
+        failed();
+      }
     }
-    void runAll() {
-        a();
-        b();
+    passed(res);
+
+    res = false;
+    // source numpy array dim (2, 4, 3)
+    vector<int> input_2({2, 2, 3, 5, 6, 6, 4, 5, 7, 5, 1, 9,
+                         1, 3, 2, 2, 5, 3, 3, 6, 3, 4, 9, 2});
+
+    // target numpy arrar (after transpose(1, 2, 0))
+    vector<int> output_2({2, 1, 2, 3, 3, 2, 5, 2, 6, 5, 6, 3,
+                          4, 3, 5, 6, 7, 3, 5, 4, 1, 9, 9, 2});
+
+    Tensor<int> inputTensor2({2, 4, 3});
+    vector<uint8_t> permute2 = {1, 2, 0};
+    permuteIndexTransform trans2(inputTensor2.getShape(), permute2);
+    for (uint32_t i = 0; i < input_2.size(); i++) {
+      testStart("test vec 2 for transform");
+      o = trans2[i];
+      res = testval(input_2[i], output_2[o]);
+      if (!res) {
+        failed();
+      }
     }
+    passed(res);
+    res = false;
+
+    vector<int> input_3({8, 6, 0, 1, 3, 9, 4, 7, 3, 2, 0, 4, 0, 9,
+                         0, 6, 0, 6, 8, 6, 8, 3, 2, 4, 2, 7, 8});
+
+    vector<int> output_3({8, 2, 8, 1, 0, 3, 4, 6, 2, 6, 0, 6, 3, 9,
+                          2, 7, 0, 7, 0, 4, 8, 9, 0, 4, 3, 6, 8});
+
+    Tensor<int> inputTensor3({1, 3, 3, 3});
+    vector<uint8_t> permute3 = {0, 3, 2, 1};
+    permuteIndexTransform trans3(inputTensor3.getShape(), permute3);
+    for (uint32_t i = 0; i < input_3.size(); i++) {
+      testStart("test vec 4d for transform");
+      o = trans3[i];
+      res = testval(input_3[i], output_3[o]);
+      if (!res) {
+        failed();
+      }
+    }
+    passed(res);
+  }
+  void runAll() {
+    runShapeTest();
+    runPermuteTest();
+  }
 };
-/*class tensorImplTest : public Test {
-public:
-    void aTest(void) {
-        testStart("a");
 
-        A *a = new A();
-        timer_start();
-        for (int i = 0; i < 100; i+=10) {
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        a->impl();
-        }
-        timer_stop();
-        double result = 0.0;
-        //passed(result < 0.0001);
-        passed(result == 0);
-    }
-    void cTest() {
-      testStart("b");
-      B *c = new C();
-      timer_start();
-      for (int i = 0; i < 100; i+=10) {
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      c->impl();
-      }
-      timer_stop();
-      double result = 0.0;
-      passed(result == 0);
-    }
-    void dTest() {
-      testStart("d");
-      D *d = new D();
-      timer_start();
-      for (int i = 0; i < 100; i+=10) {
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      d->impl();
-      }
-      timer_stop();
-      double result = 0.0;
-      passed(result == 0);
-    }
-
-    void runAll(void) {
-        aTest();
-        cTest();
-        dTest();
-    }
-};*/
-
-
-#endif //UTENSOR_impl_TESTS
+#endif  // UTENSOR_impl_TESTS
