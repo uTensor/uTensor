6f4684450df036eebb8544321c9ce001e77ba98b
kazami <kazamizero@gmail.com>
2017-11-04 22:21:45 +0800

    1. make tensor_test pass for new tensor

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "ArrayTests.hpp"
 #include "MathTests.hpp"
 #include "NnTests.hpp"
+#include "tensor_test.hpp"
 //#include "deep_mnist_mlp.hpp"
 
 Serial pc(USBTX, USBRX, 115200);
@@ -55,6 +56,11 @@ int main(int argc, char** argv) {
    printf("Nn Ops result...\r\n");
    nnTest.printSummary();
 
+   printf("Trans test: \r\n");
+   transTest tTest;
+   tTest.runAll();
+   printf("Trans result...\r\n");
+   tTest.printSummary();
   Context ctx;
   //In [24]: tf.get_default_graph().get_tensor_by_name("import/y_pred:0").eval(feed_dict={x: mnist.test.images[0:1]})
   //Out[24]: array([7])
