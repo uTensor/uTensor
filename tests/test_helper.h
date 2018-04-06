ed691c0d4fb6ff2bc87e2e5f9a1af78733161fcd
Michael Bartling <michael.bartling@arm.com>
2018-04-06 11:46:34 -0500

    Fix 64bit machine error calcs in testing

--- tests/test_helper.h
+++ tests/test_helper.h
@@ -1,6 +1,7 @@
 #ifndef __TEST_HELPER_H__
 #define __TEST_HELPER_H__
 #include "unity.h"
+#include "uTensor/core/context.hpp"
 
 #ifdef COMPILE_GREENTEA
 // Handle all the boilerplate code
@@ -54,5 +55,75 @@ utest::v1::status_t greentea_setup(const size_t number_of_cases) {
     }
 
 #endif
+
+// Math functions go here TODO move me
+//
+template <typename U>
+double meanAbsErr(Tensor* A, Tensor* B) {
+  uint32_t size_A, size_B;
+  size_A = A->getSize();
+  size_B = B->getSize();
+  if (A->getSize() != B->getSize()) {
+    /* %lu is different for 64bit machines. Need to make this cross platform
+    Shape shape_A, shape_B;
+    shape_A = A->getShape();
+    shape_B = B->getShape();
+    printf("shape_A: ");
+    for (auto s : shape_A) {
+      printf("%lu, ", s);
+    }
+    printf("\n");
+    printf("shape_B: ");
+    for (auto s : shape_B) {
+      printf("%lu, ", s);
+    }
+    printf("\n");
+    printf("size A: %lu, size B: %lu\r\n", size_A, size_B);
+    */
+    ERR_EXIT("Test.meanAbsErr(): dimension mismatch\r\n");
+  }
+
+  const U* elemA = A->read<U>(0, 0);
+  const U* elemB = B->read<U>(0, 0);
+
+  double accm = 0.0;
+  for (uint32_t i = 0; i < A->getSize(); i++) {
+    accm += (double)fabs((float)elemB[i] - (float)elemA[i]);
+  }
+
+  return accm;
+}
+
+// A being the reference
+template <typename U>
+double sumPercentErr(Tensor* A, Tensor* B) {
+  uint32_t size_A, size_B;
+  size_A = A->getSize();
+  size_B = B->getSize();
+  if (A->getSize() != B->getSize()) {
+    
+    ERR_EXIT("Test.sumPercentErr(): dimension mismatch\r\n");
+  }
+
+
+  double accm = 0.0;
+  for (uint32_t i = 0; i < A->getSize(); i++) {
+  const U* elemA = A->read<U>(i, 1);
+  const U* elemB = B->read<U>(i, 1);
+    if (elemA[0] != 0.0f) {
+      accm += (double)fabs(((float)elemB[0] - (float)elemA[0]) / ((float)elemA[0]));
+    } else {
+      if (elemB[0] != 0) {
+        accm += std::numeric_limits<float>::quiet_NaN();
+      }
+    }
+  }
+  return accm;
+}
+template<typename U>
+double meanPercentErr(Tensor* A, Tensor* B) {
+  double sum = sumPercentErr<U>(A, B);
+  return sum / A->getSize();
+}
 
 #endif
