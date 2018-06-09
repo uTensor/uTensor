353126cc47d047a4fe64bd3a9b1a5b7a532e7a71
Michael Bartling <michael.bartling@arm.com>
2018-06-09 08:59:41 +0800

    Fist draft of cmsis FC ops

---
+++ uTensor/ops/cmsis_ops/FullyConnectedOps.hpp
@@ -0,0 +1,153 @@
+#ifndef UTENSOR_FULLY_CONNECTED_OPS
+#define UTENSOR_FULLY_CONNECTED_OPS
+#include "uTensor/core/tensor.hpp"
+#include "uTensor/core/uTensorBase.hpp"
+#include "arm_math.h"
+#include "arm_nnfunctions.h"
+
+/***
+ * Here we have two types of kernel functions. The basic function implements the function using regular GEMV approach. The opt functions operates with weights in interleaved formats.
+ *
+ * http://www.keil.com/pack/doc/CMSIS/NN/html/group__FC.html#gae3857bb6375692e81dde8cbd70adec08
+ */
+
+/**
+ * @param [in] iV input vector
+ * @param [in] mW matrix weights
+ * @param [in] b bias
+ * @param [in] scratch scrath computation space
+ * @param [out] pOut output
+ */
+template<typename T1, typename T2, typename TOut>
+void FullyConnectedLayerCmsis(S_TENSOR iV, S_TENSOR mW, S_TENSOR b,
+                         S_TENSOR bShift, S_TENSOR oShift,
+                         S_TENSOR pOut, S_TENSOR scratch)
+{
+    //Throw error if this gets called
+}
+
+template<>
+void FullyConnectedLayerCmsis<q7_t, q7_t, q7_t>(S_TENSOR iV, S_TENSOR mW, S_TENSOR b,
+                                           S_TENSOR bShift, S_TENSOR oShift,
+                                           S_TENSOR pOut, S_TENSOR scratch)
+{
+    const q7_t* iV_data = iV->read<q7_t>(0, sizeof(q7_t)); //Read one byte
+    const q7_t* mW_data = mW->read<q7_t>(0, sizeof(q7_t)); //Read one byte
+    const uint16_t dim_vec = iV->getShape()[0];
+    const uint16_t num_of_rows = mW->getShape()[0];
+    const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
+    const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
+    q7_t* pOut_data = pOut->write(0, sizeof(q7_t));
+    q15_t* scratch_data = scratch->write(0, sizeof(q15_t));
+
+    arm_fully_connected_q7(iV_data, mW_data, dim_vec, dim_vec, 
+            num_of_rows, bias_shift, out_shift, pOut_data, scratch_data);
+    //Error checking
+}
+template<>
+void FullyConnectedLayerCmsis<q15_t, q15_t, q15_t>(S_TENSOR iV, S_TENSOR mW, S_TENSOR b,
+                                           S_TENSOR bShift, S_TENSOR oShift,
+                                           S_TENSOR pOut, S_TENSOR scratch)
+{
+    const q15_t* iV_data = iV->read<q15_t>(0, sizeof(q15_t)); //Read one byte
+    const q15_t* mW_data = mW->read<q15_t>(0, sizeof(q15_t)); //Read one byte
+    const uint16_t dim_vec = iV->getShape()[0];
+    const uint16_t num_of_rows = mW->getShape()[0];
+    const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
+    const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
+    q15_t* pOut_data = pOut->write(0, sizeof(q15_t));
+    q15_t* scratch_data = scratch->write(0, sizeof(q15_t));
+
+    arm_fully_connected_q15(iV_data, mW_data, dim_vec, dim_vec, 
+            num_of_rows, bias_shift, out_shift, pOut_data, scratch_data);
+    //Error checking
+}
+
+/***
+ * Here we have two types of kernel functions. The basic function implements the function using regular GEMV approach. The opt functions operates with weights in interleaved formats.
+ *
+ * http://www.keil.com/pack/doc/CMSIS/NN/html/group__FC.html#gae3857bb6375692e81dde8cbd70adec08
+ */
+
+/**
+ * @param [in] iV input vector
+ * @param [in] mW matrix weights
+ * @param [in] b bias
+ * @param [in] scratch scrath computation space
+ * @param [out] pOut output
+ */
+template<typename T1, typename T2, typename TOut>
+void FullyConnectedLayerOptCmsis(S_TENSOR iV, S_TENSOR mW, S_TENSOR b,
+                         S_TENSOR bShift, S_TENSOR oShift,
+                         S_TENSOR pOut, S_TENSOR scratch)
+{
+    //Throw error if this gets called
+}
+
+template<>
+void FullyConnectedLayerOptCmsis<q7_t, q7_t, q7_t>(S_TENSOR iV, S_TENSOR mW, S_TENSOR b,
+                                           S_TENSOR bShift, S_TENSOR oShift,
+                                           S_TENSOR pOut, S_TENSOR scratch)
+{
+    const q7_t* iV_data = iV->read<q7_t>(0, sizeof(q7_t)); //Read one byte
+    const q7_t* mW_data = mW->read<q7_t>(0, sizeof(q7_t)); //Read one byte
+    const uint16_t dim_vec = iV->getShape()[0];
+    const uint16_t num_of_rows = mW->getShape()[0];
+    const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
+    const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
+    q7_t* pOut_data = pOut->write(0, sizeof(q7_t));
+    q15_t* scratch_data = scratch->write(0, sizeof(q15_t));
+
+    arm_fully_connected_q7_opt(iV_data, mW_data, dim_vec, dim_vec, 
+            num_of_rows, bias_shift, out_shift, pOut_data, scratch_data);
+    //Error checking
+}
+template<>
+void FullyConnectedLayerOptCmsis<q15_t, q15_t, q15_t>(S_TENSOR iV, S_TENSOR mW, S_TENSOR b,
+                                           S_TENSOR bShift, S_TENSOR oShift,
+                                           S_TENSOR pOut, S_TENSOR scratch)
+{
+    const q15_t* iV_data = iV->read<q15_t>(0, sizeof(q15_t)); //Read one byte
+    const q15_t* mW_data = mW->read<q15_t>(0, sizeof(q15_t)); //Read one byte
+    const uint16_t dim_vec = iV->getShape()[0];
+    const uint16_t num_of_rows = mW->getShape()[0];
+    const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
+    const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
+    q15_t* pOut_data = pOut->write(0, sizeof(q15_t));
+    q15_t* scratch_data = scratch->write(0, sizeof(q15_t));
+
+    arm_fully_connected_q15_opt(iV_data, mW_data, dim_vec, dim_vec, 
+            num_of_rows, bias_shift, out_shift, pOut_data, scratch_data);
+    //Error checking
+}
+
+
+template <class T1, class T2, class TOut>
+class FullyConnectedLayerCmsisOp : public Operator {
+  public:
+  FullyConnectedLayerCmsisOp() {
+    n_inputs = 7;
+    n_outputs = 1;
+  }
+  virtual void compute() override {
+    FullyConnectedLayerCmsis<T1, T2, TOut>(inputs[0], inputs[1], inputs[2], inputs[3],
+      inputs[4], inputs[5], outputs[0], inputs[6]);
+  }
+};
+
+
+template <class T1, class T2, class TOut>
+class FullyConnectedLayerOptCmsisOp : public Operator {
+  public:
+  FullyConnectedLayerOptCmsisOp() {
+    n_inputs = 6;
+    n_outputs = 3;
+  }
+  virtual void compute() override {
+    FullyConnectedLayerOptCmsis<T1, T2, TOut>(inputs[0], inputs[1], inputs[2], inputs[3],
+      inputs[4], inputs[5], outputs[0], inputs[6]);
+  }
+};
+
+
+#endif /*UTENSOR_FULLY_CONNECTED_OPS*/
