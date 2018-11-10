8efd6c6011f0df0cf35cfe3d216a16c3381b68b6
Neil Tan <neil.c.tan@gmail.com>
2018-11-10 14:55:26 +0800

    - Added support op: Uint8Q7OriginOp - Added support op: QuantRangeForMultiplication - (WIP) Modifying FullyConnectedLayerCmsisOp for arm_fully_connected_q7_tout<T_OUT>
    The plan is to use CMSIS-NN FC for MatMul computation only. Bias will be added using node fusion once we varify MatMul works.

--- uTensor/ops/cmsis_ops/FullyConnectedOps.hpp
+++ uTensor/ops/cmsis_ops/FullyConnectedOps.hpp
@@ -5,6 +5,139 @@
 #include "arm_math.h"
 #include "arm_nnfunctions.h"
 
+
+#define ARM_MATH_DSP
+
+template <class T_OUT=q7_t>
+arm_status
+
+arm_fully_connected_q7_tout(const q7_t * pV,
+                       const q7_t * pM,
+                       const uint16_t dim_vec,
+                       const uint16_t num_of_rows,
+                       const uint16_t bias_shift,
+                       const uint16_t out_shift, const q7_t * bias, T_OUT * pOut, q15_t * vec_buffer)
+
+{
+#if defined (ARM_MATH_DSP)
+    /* Run the following code for Cortex-M4 and Cortex-M7 */
+
+    const q7_t *pB = pM;
+    const q7_t *pB2;
+    T_OUT     *pO = pOut;
+    q15_t    *pA;
+    uint16_t  rowCnt = num_of_rows >> 1;
+
+    /* expand the vector into the buffer */
+    arm_q7_to_q15_reordered_no_shift(pV, vec_buffer, dim_vec);
+
+    while (rowCnt)
+    {
+        q31_t     sum =  0;
+        q31_t     sum2 = 0;
+        uint16_t  colCnt = dim_vec >> 2;
+
+        pA = vec_buffer;
+        pB2 = pB + dim_vec;
+
+        while (colCnt)
+        {
+            q31_t     inV, inM11, inM12, inM21, inM22;
+            pB = (q7_t *) read_and_pad_reordered((void *)pB, &inM11, &inM12);
+            pB2 = (q7_t *) read_and_pad_reordered((void *)pB2, &inM21, &inM22);
+
+            inV = *__SIMD32(pA)++;
+
+            sum = __SMLAD(inV, inM11, sum);
+            sum2 = __SMLAD(inV, inM21, sum2);
+
+            inV = *__SIMD32(pA)++;
+
+            sum = __SMLAD(inV, inM12, sum);
+            sum2 = __SMLAD(inV, inM22, sum2);
+
+            colCnt--;
+        }
+        colCnt = dim_vec & 0x3;
+        while (colCnt)
+        {
+            q7_t      inV = *pA++;
+            q15_t     inM = *pB++;
+            q15_t     inM2 = *pB2++;
+
+            sum += inV * inM;
+            sum2 += inV * inM2;
+            colCnt--;
+        }                       /* while over colCnt */
+        *pO++ = sum;
+        *pO++ = sum2;
+
+        /* adjust the pointers and counters */
+        pB += dim_vec;
+        rowCnt--;
+    }
+
+    /* left-over part of the rows */
+    rowCnt = num_of_rows & 0x1;
+
+    while (rowCnt)
+    {
+        uint16_t  colCnt = dim_vec >> 2;
+        q31_t     sum = 0;
+
+        pA = vec_buffer;
+
+        while (colCnt)
+        {
+            q31_t     inV1, inV2, inM11, inM12;
+
+            pB = (q7_t *) read_and_pad_reordered((void *)pB, &inM11, &inM12);
+
+            inV1 = *__SIMD32(pA)++;
+            sum = __SMLAD(inV1, inM11, sum);
+
+            inV2 = *__SIMD32(pA)++;
+            sum = __SMLAD(inV2, inM12, sum);
+
+            colCnt--;
+        }
+
+        /* left-over of the vector */
+        colCnt = dim_vec & 0x3;
+        while (colCnt)
+        {
+            q7_t      inV = *pA++;
+            q15_t     inM = *pB++;
+            sum += inV * inM;
+            colCnt--;
+        }
+
+        *pO++ = sum;
+
+        rowCnt--;
+    }
+
+#else
+    int       i, j;
+
+    /* Run the following code as reference implementation for Cortex-M0 and Cortex-M3 */
+    for (i = 0; i < num_of_rows; i++)
+    {
+        int       ip_out = 0;
+        for (j = 0; j < dim_vec; j++)
+        {
+            ip_out += pV[j] * pM[i * dim_vec + j];
+        }
+        pOut[i] = (T_OUT) ip_out;
+    }
+
+#endif                          /* ARM_MATH_DSP */
+
+    /* Return to ARM_MATH_SUCCESS */
+    return (ARM_MATH_SUCCESS);
+
+}
+
 /***
  * Here we have two types of kernel functions. The basic function implements the function using regular GEMV approach. The opt functions operates with weights in interleaved formats.
  *
@@ -12,6 +145,14 @@
  */
 
 //I definitely overcomplicated this part. Should just use f*** function overloading
+void cmsis_fc_selector(const q7_t* iV, const q7_t* mW, const uint16_t dim_vec, const uint16_t num_of_rows,
+                       const uint16_t bias_shift, const uint16_t out_shift, const q7_t* bias, 
+                       uint32* pOut, q15_t* scratch_data)
+{
+    arm_fully_connected_q7_tout(iV, mW, dim_vec, num_of_rows, 
+            bias_shift, out_shift, bias, pOut, scratch_data);
+}
+
 void cmsis_fc_selector(const q7_t* iV, const q7_t* mW, const uint16_t dim_vec, const uint16_t num_of_rows,
                        const uint16_t bias_shift, const uint16_t out_shift, const q7_t* bias, 
                        q7_t* pOut, q15_t* scratch_data)
@@ -82,7 +223,7 @@ struct FullyConnectedLayerCmsis {
         const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
         const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
         pOut->resize(b->getShape());
-        T1* pOut_data = pOut->write<T1>(0, sizeof(T1));
+        T1* pOut_data = pOut->write<T1>(0, sizeof(T1)); //FIXME: check the definiion of write()
         q15_t* scratch_data = scratch->write<q15_t>(0, sizeof(q15_t));
     
         cmsis_fc_selector(iV_data, mW_data, dim_vec, num_of_rows, 
@@ -121,7 +262,7 @@ struct FullyConnectedLayerOptCmsis{
         const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
         const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
         pOut->resize(b->getShape());
-        T1* pOut_data = pOut->write<T1>(0, sizeof(T1));
+        T1* pOut_data = pOut->write<T1>(0, sizeof(T1));  //FIXME: check the definiion of write()
         q15_t* scratch_data = scratch->write<q15_t>(0, sizeof(q15_t));
     
         cmsis_fc_selector_opt(iV_data, mW_data, dim_vec, num_of_rows, 
@@ -132,8 +273,23 @@ struct FullyConnectedLayerOptCmsis{
     }
 };
 
-//TARGET_OP={FullyConnectedLayerCmsis, FullyConnectedLayerOptCmsis}
-template <class T1, class T2, class T3, class TARGET_OP=FullyConnectedLayerCmsis>
+// //TARGET_OP={FullyConnectedLayerCmsis, FullyConnectedLayerOptCmsis}
+// template <class T1, class T2, class T3, class TARGET_OP=FullyConnectedLayerCmsis>
+// class FullyConnectedLayerCmsisOp : public Operator {
+//   public:
+//   FullyConnectedLayerCmsisOp() {
+//     n_inputs = 6;
+//     n_outputs = 1;
+//   }
+//   virtual void compute() override {
+//     T1 x;
+//     T2 y;
+//     T3 byteme;
+//     TARGET_OP()(inputs[0], inputs[1], inputs[2], inputs[3],
+//       inputs[4], outputs[0], inputs[5], x, y, byteme);
+//   }
+// };
+
 class FullyConnectedLayerCmsisOp : public Operator {
   public:
   FullyConnectedLayerCmsisOp() {
@@ -141,13 +297,30 @@ class FullyConnectedLayerCmsisOp : public Operator {
     n_outputs = 1;
   }
   virtual void compute() override {
-    T1 x;
-    T2 y;
-    T3 byteme;
-    TARGET_OP()(inputs[0], inputs[1], inputs[2], inputs[3],
-      inputs[4], outputs[0], inputs[5], x, y, byteme);
+    S_TENSOR iV = inputs[0];
+    S_TENSOR mW = inputs[1];
+    S_TENSOR b = inputs[2];
+    S_TENSOR bShift = inputs[3];
+    S_TENSOR oShift = inputs[4];
+    S_TENSOR pOut = outputs[0];
+    S_TENSOR scratch = inputs[5];
+
+    const q7_t* iV_data  = iV->read<q7_t>(0, 1); //Read one byte
+    const q7_t* mW_data  = mW->read<q7_t>(0, 1); //Read one byte
+    const q7_t* bias_data = b->read<q7_t>(0, 1); //Read one byte
+    const uint16_t dim_vec = iV->getShape()[0];
+    const uint16_t num_of_rows = mW->getShape()[0];
+    const uint16_t bias_shift = *(bShift->read<uint16_t>(0,0));
+    const uint16_t out_shift = *(oShift->read<uint16_t>(0,0));
+    pOut->resize(b->getShape());
+        int32_t* pOut_data = pOut->write<int32_t>(0, 1);
+        q15_t* scratch_data = scratch->write<q15_t>(0, 1);
+    
+        arm_fully_connected_q7_tout<T_OUT>(iV_data, mW_data, dim_vec, num_of_rows, 
+            bias_shift, out_shift, bias_data, pOut_data, scratch_data);
   }
 };
+
 
 
 #endif /*UTENSOR_FULLY_CONNECTED_OPS*/
---
+++ uTensor/ops/cmsis_ops/supportOps.hpp
@@ -0,0 +1,76 @@
+#ifndef UTENSOR_SUPPORT_OPS
+#define UTENSOR_SUPPORT_OPS
+#include "uTensor/core/tensor.hpp"
+#include "uTensor/core/uTensorBase.hpp"
+#include "uTensor/util/quantization_utils.hpp"
+#include "arm_math.h"
+#include "arm_nnfunctions.h"
+
+void uint8_to_q7_origin(uint8_t *input, float &min, float &max, q7_t *out, size_t length) {
+  //NT TODO: consider loop unrolling or SIMD here
+  const int32_t offset = FloatToQuantizedUnclamped<uint8_t>(
+      0.0f, min, max) - 128;
+  for(size_t i = 0; i < length; i++) {
+    out[i] = (q7_t) (input[i] - offset);
+  }
+}
+
+class Uint8Q7OriginOp : public Operator {
+  public:
+  Uint8Q7OriginOp() {
+    n_inputs = 3;
+    n_outputs = 1;
+  }
+  virtual void compute() override {
+    uint8_t *input = inputs[0]->read<uint8_t>(0, 1);
+    float min = inputs[1]->read<float>(0, 1);
+    float max = inputs[2]->read<float>(0, 1);
+
+    if(outputs[0]->getSize() == 0) {
+      Shape out_shape;
+      out_shape.push_back((inputs[0]->getShape())[0]);
+      outputs[0]->resize(out_shape);
+    }
+
+    q7_t *out = outputs[0]->write<q7_t>(0, 1);
+
+    uint8_to_q7_origin(inputs[0], min, max, out, inputs[0]->getSize());
+  }
+};
+
+template <class T1, class T2, class Toutput>
+class QuantRangeForMultiplication : public Operator {
+  public:
+  QuantRangeForMultiplication() {
+    n_inputs = 4;
+    n_outputs = 2;
+  }
+  virtual void compute() override {
+    float min_a = inputs[0]->read<float>(0, 1);
+    float max_a = inputs[1]->read<float>(0, 1);
+    float min_b = inputs[2]->read<float>(0, 1);
+    float max_b = inputs[3]->read<float>(0, 1);
+
+    if(outputs[0]->getSize() == 0) {
+      Shape out_shape;
+      out_shape.push_back(1);
+      outputs[0]->resize(out_shape);
+    }
+
+    if(outputs[1]->getSize() == 0) {
+      Shape out_shape;
+      out_shape.push_back(1);
+      outputs[1]->resize(out_shape);
+    }
+
+    q7_t *out = outputs[0]->write<q7_t>(0, 1);
+
+    float *min_c_value = outputs[0]->write<float>(0, 1);
+    float *max_c_value = outputs[1]->write<float>(0, 1);
+
+    QuantizationRangeForMultiplication<T1, T2, Toutput>(
+      min_a, max_a, min_b, max_b, min_c_value, max_c_value);
+  }
+};
+
+#endif
