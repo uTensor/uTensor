7567e8bee453e65f4a8ae0ff6459dd44f2e3688b
Michael Bartling <michael.bartling@arm.com>
2019-11-20 09:01:58 -0600

    Move raw data handle to it's own thing

--- src/uTensor/core/memoryManagementInterface.hpp
+++ src/uTensor/core/memoryManagementInterface.hpp
@@ -24,28 +24,6 @@ class Handle {
   void* _ptr;
 };
 
-class RawDataHandle : public Handle {
- public:
-  RawDataHandle(size_t req_ram_size) {
-    _ptr = Context::DefaultRamDataAllocator::allocate(req_ram_size);
-    Context::DefaultRamDataAllocator::bind(this, _ptr);
-  }
-  ~RawDataHandle() { Context::DefaultRamDataAllocator::deallocate(_ptr); }
-  // void* operator new(size_t sz) { // Have to delegate this size from tensors
-  // somehow + sizeof(Tensor)
-  //  void* p = Context::DefaultTensorMetaDataAllocator::allocate(sz);
-  //  return p;
-  //}
-  // void operator delete(void* p) {
-  //  Context::DefaultTensorMetaDataAllocator::deallocate(p);
-  //}
-  // Ignore this, use the constructor to specify the data space
-  //  void* operator new(size_t sz, size_t req_ram_size){
-  //    void* p = Context::DefaultTensorMetaDataAllocator::allocate(sz);
-  //    _ptr = Context::DefaultRamDataAllocator::allocator(req_ram_size);
-  //    return p;
-  //  }
-};
 /**
  * Allocators are expected to maintain a mapping of Tensor handles to data
  * regions. * This allows the allocator to move around the underlying data
