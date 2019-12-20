548b4943ebd40ce7865d7ec87688328bc918a628
Michael Bartling <michael.bartling@arm.com>
2019-12-20 12:23:13 -0600

    Add bulk of functionality to circ buffer

--- src/uTensor/allocators/arenaAllocator.hpp
+++ src/uTensor/allocators/arenaAllocator.hpp
@@ -16,6 +16,7 @@ template<size_t size>
 class localCircularArenaAllocator : public AllocatorInterface {
   private:
     class MetaHeader {
+      public:
         uint16_t meta_data;
         Handle* hndl;
       public:
@@ -25,9 +26,10 @@ class localCircularArenaAllocator : public AllocatorInterface {
         void set_inactive() { meta_data &= (BLOCK_ACTIVE | 0x7FFF) ; }
         void set_hndl(Handle* handle) { hndl = handle; }
         void set_len(uint16_t sz) { meta_data &= 0x8000; meta_data |= (0x7FFF & sz); }
+        uint16_t get_len() const { return meta_data & 0x7FFF; }
         bool is_active() const { return (meta_data & 0x8000) == BLOCK_ACTIVE; }
         bool is_bound() const { return (hndl != nullptr); }
-        bool has_handle(Handle* target) const { return hndl == target; }
+        bool has_handle(Handle* target) const { return is_active && (hndl == target); }
     }
   private:
     uint16_t capacity;
@@ -57,6 +59,9 @@ class localCircularArenaAllocator : public AllocatorInterface {
       uint8_t* p = reinterpret_cast<uint8_t*>(ptr);
       memcpy(p - sizeof(MetaHeader), &hdr, sizeof(MetaHeader));
     }
+
+    uint8_t* begin() const { return _buffer + sizeof(MetaHeader); }
+    uint8_t* end() const { return _buffer + size; }
   protected:
     virtual void _bind(void* ptr, Handle* hndl){
       MetaHeader hdr = _read_header(ptr);
@@ -70,6 +75,7 @@ class localCircularArenaAllocator : public AllocatorInterface {
 
     virtual void _unbind(void* ptr, Handle* hndl){
       //teehee
+      hndl->_ptr = nullptr;
       _bind(ptr, nullptr);
     }
 
@@ -82,12 +88,86 @@ class localCircularArenaAllocator : public AllocatorInterface {
       return hdr.is_bound();
     }
 
-    virtual bool _has_handle(Handle* hndl) = 0;
-    virtual void* _allocate(size_t sz) = 0;
-    virtual void _deallocate(void* ptr) = 0;
+    virtual bool _has_handle(Handle* hndl){
+      uint8_t* m_cursor = begin();
+      while(!(m_cursor > end())) {
+        MetaHeader hdr = _read_header((void*) m_cursor);
+        if(hdr.has_handle(hndl)){
+          return true;
+        }
+        m_cursor += hdr.get_len();
+      }
+      return false;
+    }
+
+    virtual void* _allocate(size_t sz){
+      uint8_t* loc = nullptr;
+      // If make this capacity then have possibility of filling up
+      if(sz > size){
+        //ERROR
+      }
+      if(sz > ( (_buffer + size) - cursor)){
+        //Allocate at beginning
+        // Rebalance to make it less likely to overwrite a region
+        // Overwriting allocated regions is a valid operation as long as the overwritten regions are invalidated
+        rebalance();
+      }
+      MetaHeader hdr = _read_header((void*) cursor);
+      if(hdr.is_active() || (hdr.get_len() > 0 && hdr.get_len() < sz)){
+        //clear necessary chunks until enough space for current request + appended fragment header
+        uint8_t* forward_cursor = cursor;
+        while((forward_cursor - cursor) < sz){
+          MetaHeader f_hdr = _read_header((void*) forward_cursor);
+          deallocate((void*) forward_cursor);
+        // Decide whether we need to insert a fragment header or not
+          forward_cursor += f_hdr.get_len() + sizeof(MetaHeader);
+          if((forward_cursor - cursor) > (sz + sizeof(MetaHeader))){
+            f_hdr.set_inactive();
+            f_hdr.set_hndl(nullptr);
+            // set it to the free length
+            f_hdr.set_len( forward_cursor - sizeof(MetaHeader) - cursor + sz);
+            _write_header( f_hdr, (void*)(cursor + sz));
+          }
+        }
+      }
+      hdr.set_active();
+      hdr.set_len(sz);
+      hdr.set_hndl(nullptr);
+      _write_header(hdr, (void*) cursor);
+      loc = cursor;
+      cursor += hdr.get_len() + sizeof(MetaHeader);
+      return (void*)loc;
+    }
+    virtual void _deallocate(void* ptr){
+      if(ptr){
+        MetaHeader hdr = _read_header(ptr);
+        hdr.set_inactive();
+        if(hdr.is_bound()){
+          _unbind(ptr, hdr.hndl);
+        }
+        _write_header(hdr, ptr);
+      }
+    }
 
   public:
-    localCircularArenaAllocator() : capacity(size) {}
+    localCircularArenaAllocator() : capacity(size) {
+      memset(_buffer, 0, size);
+      cursor = begin();
+    }
+
+    /** This implementation of rebalance shifts all allocated chunks to the end of the buffer and inserts an inactive region at the start.
+     * note: cursor gets moved to begin()
+     */
+    virtual bool rebalance() {
+      //TODO WARNING rebalancing Allocator
+      // Shift each chunk towards the end of the buffer
+      
+      // Write new header to start
+      cursor = begin();
+      MetaHeader hdr(empty_chunk_len);
+      hdr.set_inactive();
+      _write_header(hdr, (void*)cursor);
+    }
 
 };
 
