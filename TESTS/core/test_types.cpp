4bd282832788bf6ec28f9d8bb40f8c5478b77907
Michael Bartling <michael.bartling@arm.com>
2019-12-31 11:58:59 -0600

    Fix TensorShape and test

--- TESTS/core/test_types.cpp
+++ TESTS/core/test_types.cpp
@@ -1,5 +1,27 @@
 #include "gtest/gtest.h"
+#include "types.hpp"
 
-TEST(Sanity, test_1) {
-  ASSERT_EQ(true, true);
+
+TEST(Shapes, test_1) {
+  TensorShape s1(10);
+  EXPECT_EQ(s1.num_dims(), 1);
+  EXPECT_EQ(s1[0], 10);
+}
+
+TEST(Shapes, test_2d) {
+  TensorShape s1(10, 10);
+  EXPECT_EQ(s1.num_dims(), 2);
+  EXPECT_EQ(s1[0], 10);
+  EXPECT_EQ(s1[1], 10);
+  EXPECT_EQ(s1.linear_index(1,1,0,0), 11);
+}
+
+TEST(Shapes, test_3d) {
+  TensorShape s1(10, 10, 10);
+  EXPECT_EQ(s1.num_dims(), 3);
+  EXPECT_EQ(s1[0], 10);
+  EXPECT_EQ(s1[1], 10);
+  EXPECT_EQ(s1[2], 10);
+  EXPECT_EQ(s1.linear_index(1,1,0,0), 11);
+  EXPECT_EQ(s1.linear_index(1,1,1,0), 111);
 }
