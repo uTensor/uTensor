3b562bbd072902fdd4e326f88eb3e2b9ed1ce813
Michael Bartling <michael.bartling@arm.com>
2020-01-02 16:19:57 -0600

    Add more RamTensor Tests

--- TESTS/tensors/test_ramtensor.cpp
+++ TESTS/tensors/test_ramtensor.cpp
@@ -37,3 +37,30 @@ TEST(RAM_Tensor, read_write_u8) {
   EXPECT_EQ(read, 5);
   cout << "Sizeof IntegralValue " << sizeof(IntegralValue(5)) << endl;
 }
+
+TEST(RAM_Tensor, read_write_u8_2) {
+  ///setup_context();
+  localCircularArenaAllocator<256> meta_allocator;
+  localCircularArenaAllocator<256> ram_allocator;
+  Context::set_metadata_allocator(&meta_allocator);
+  Context::set_ram_data_allocator(&ram_allocator);
+  RamTensor r({10, 10}, u8);
+  r(2,2) = (uint8_t) 5;
+  uint8_t read = r(2,2);
+  EXPECT_EQ(read, 5);
+  r(2,2) = (uint8_t) 15;
+  EXPECT_EQ((uint8_t)r(2,2), 15);
+}
+
+TEST(RAM_Tensor, read_write_i8) {
+  ///setup_context();
+  localCircularArenaAllocator<256> meta_allocator;
+  localCircularArenaAllocator<256> ram_allocator;
+  Context::set_metadata_allocator(&meta_allocator);
+  Context::set_ram_data_allocator(&ram_allocator);
+  RamTensor r({10, 10}, i8);
+  r(2,2) = (int8_t) -5;
+  int8_t read = r(2,2);
+  EXPECT_EQ(read, -5);
+  cout << "Sizeof IntegralValue " << sizeof(IntegralValue(5)) << endl;
+}
