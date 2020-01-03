13f2fe638d0a58a59d7240e459fad31a3d01dde8
Michael Bartling <michael.bartling@arm.com>
2020-01-03 11:30:41 -0600

    Working, but need to fix the read/write function selection logic

--- src/uTensor/tensors/RomTensor.cpp
+++ src/uTensor/tensors/RomTensor.cpp
@@ -4,9 +4,12 @@ namespace uTensor {
 RomTensor::RomTensor(TensorShape _shape, ttype _type, const void* buffer) : 
     BufferTensor(_shape, _type, const_cast<void*>(buffer))
 { }
+
+// TODO Need to fix the write/read selection functions
 void* RomTensor::write(uint32_t linear_index) {
-    printf("[ERROR] Attempted write to ROM tensor\n");
-    return nullptr;
+    //printf("[ERROR] Attempted write to ROM tensor\n");
+    //return nullptr;
+    return BufferTensor::write(linear_index);
 }
 
 RomTensor::~RomTensor() { }
