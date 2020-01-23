99a984bb507dfc653c43d0aac1f84bb803559de8
Michael Bartling <michael.bartling@arm.com>
2020-01-23 10:58:17 -0600

    Add basic PowerSpectrum op skeleton for x86

--- src/uTensor/ops/optimized/x86/FFT.hpp
+++ src/uTensor/ops/optimized/x86/FFT.hpp
@@ -1,7 +1,22 @@
 #ifndef UTENSOR_FFT_HPP
 #define UTENSOR_FFT_HPP
 
-namespace uTensor {
+#include "fftw3.h"
 
+namespace uTensor {
+  //fftw_plan fftw_plan_dft_r2c_1d(int n, double *in, fftw_complex *out, unsigned flags);
+
+void power_spectrum_kernel(Tensor& power, const Tensor& input) {
+
+}
+class PowerSpectrum : public OperatorInterface<1,1> {
+  public:
+    enum names: uint8_t { input, power };
+
+  protected:
+    virtual void compute() {
+
+    }
+};
 }
 #endif
