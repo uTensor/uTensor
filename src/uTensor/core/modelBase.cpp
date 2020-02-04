4d6ac7ce091719549a46e9619ac925f9951419d9
Michael Bartling <michael.bartling@arm.com>
2020-02-04 12:24:42 -0600

    Update build system

--- src/uTensor/core/modelBase.cpp
+++ src/uTensor/core/modelBase.cpp
@@ -10,12 +10,19 @@ namespace uTensor {
 //    //        ctx.push_op_tensors(*this, outputs);
 //}
 // The preferred interface
+ModelBase::ModelBase() {}
+ModelBase::ModelBase(uTensor::string _name) : _name(_name) {}
+void ModelBase::set_name(uTensor::string _name) { this->_name = _name; }
+
+ModelBase::~ModelBase() {
+  Context* ctx = Context::get_default_context();
+  // ctx.pop_op_tensors(*this, inputs); // Inputs are no longer needed
+}
+void ModelBase::eval() { compute(); }
+
 ModelBase::ModelBase(TensorMapInterface* inputs) : _p_inputs(inputs) {
   // Context* ctx = Context::get_default_context();
 }
-ModelBase::ModelBase() {}
-ModelBase::ModelBase(uTensor::string _name) : _name(_name) {}
-
 void ModelBase::set_inputs(TensorMapInterface* inputs) {
   this->_p_inputs = inputs;
   // Context* ctx = Context::get_default_context();
@@ -27,10 +34,4 @@ void ModelBase::set_outputs(TensorMapInterface* outputs) {
   // ctx.push_op_tensors(*this, outputs);
 }
 
-void ModelBase::eval() { compute(); }
-ModelBase::~ModelBase() {
-  Context* ctx = Context::get_default_context();
-  // ctx.pop_op_tensors(*this, inputs); // Inputs are no longer needed
-}
-void ModelBase::set_name(uTensor::string _name) { this->_name = _name; }
 }  // namespace uTensor
