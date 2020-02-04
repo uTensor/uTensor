be51fbbdfcfae8d7d523a858eea878318fc6e9dc
Michael Bartling <michael.bartling@arm.com>
2020-02-04 16:31:01 -0600

    Add simple print

--- src/uTensor/core/tensor.cpp
+++ src/uTensor/core/tensor.cpp
@@ -1,5 +1,6 @@
 #include "tensor.hpp"
 #include "context.hpp"
+#include "uTensor_util.hpp"
 
 namespace uTensor {
 // Tensor::Tensor(const Tensor& that) {} // Cannot copy Tensors, must pass by
@@ -99,6 +100,50 @@ TensorShape& Tensor::get_shape() {
 
 TensorInterface* TensorReference::operator*() {
   return reinterpret_cast<TensorInterface*>(_ref->operator*());
+}
+
+void print(const Tensor& t) {
+  const TensorShape& t_shape = t->get_shape();
+  if(t_shape.num_dims() > 2){
+    uTensor_printf("printing > 2D tensors not supported\n");
+    return;
+  }
+  uTensor_printf("[\n");
+  for (int j = 0; j < t_shape[1]; j++) {
+    uTensor_printf("[ ");
+    for (int i = 0; i < t_shape[0]; i++) {
+      switch(t->get_type()) {
+        case u8:
+          uTensor_printf("%hhu", static_cast<uint8_t>(t(j,i)));
+          break;
+        case i8:
+          uTensor_printf("%hhd", static_cast<int8_t>(t(j,i)));
+          break;
+        case u16:
+          uTensor_printf("%hu", static_cast<uint16_t>(t(j,i)));
+          break;
+        case i16:
+          uTensor_printf("%hd", static_cast<int16_t>(t(j,i)));
+          break;
+        case u32:
+          uTensor_printf("%u", static_cast<uint32_t>(t(j,i)));
+          break;
+        case i32:
+          uTensor_printf("%d", static_cast<int32_t>(t(j,i)));
+          break;
+        case flt:
+          uTensor_printf("%f", static_cast<float>(t(j,i)));
+          break;
+      }
+      if( i != (t_shape[0]-1) )
+        uTensor_printf(", ");
+      else
+        uTensor_printf(" ");
+      
+    }
+    uTensor_printf("]\n");
+  }
+    uTensor_printf("]\n");
 }
 
 SimpleNamedTensor::SimpleNamedTensor(const uTensor::string& name,
