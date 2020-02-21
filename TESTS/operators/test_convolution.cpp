833d12602ea3e6a798cea924ad4094e478218295
Michael Bartling <michael.bartling@arm.com>
2020-02-21 13:09:21 -0600

    Add increased indexing size type to conv tests

--- TESTS/operators/test_convolution.cpp
+++ TESTS/operators/test_convolution.cpp
@@ -219,7 +219,7 @@ TEST(Convolution, random_inputs_1_stride_2) {
 
 TEST(Convolution, random_inputs_2_stride_2) {
   localCircularArenaAllocator<1024> meta_allocator;
-  localCircularArenaAllocator<6272*2*sizeof(float)> ram_allocator;
+  localCircularArenaAllocator<6272*2*sizeof(float), uint32_t> ram_allocator;
   Context::get_default_context()->set_metadata_allocator(&meta_allocator);
   Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
 
@@ -244,7 +244,7 @@ TEST(Convolution, random_inputs_2_stride_2) {
 
 TEST(Convolution, random_inputs_3_stride_2) {
   localCircularArenaAllocator<1024> meta_allocator;
-  localCircularArenaAllocator<6272*2*sizeof(float)> ram_allocator;
+  localCircularArenaAllocator<6272*2*sizeof(float), uint32_t> ram_allocator;
   Context::get_default_context()->set_metadata_allocator(&meta_allocator);
   Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
 
@@ -288,4 +288,275 @@ TEST(Convolution, random_inputs_4_stride_2) {
     EXPECT_NEAR((float) out(i), s_ref_out_4_stride_2[i], 0.0001);
   }
 }
+
+
 #endif
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_0_stride_1) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<18432*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_0_stride_1);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_0_stride_1);
+  Tensor out = new RamTensor({ 1,24,24,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,1,1,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 18432; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_0_stride_1[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_0_stride_2) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<4608*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_0_stride_2);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_0_stride_2);
+  Tensor out = new RamTensor({ 1,12,12,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,2,2,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 4608; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_0_stride_2[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_1_stride_1) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<18432*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_1_stride_1);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_1_stride_1);
+  Tensor out = new RamTensor({ 1,24,24,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,1,1,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 18432; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_1_stride_1[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_1_stride_2) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<4608*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_1_stride_2);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_1_stride_2);
+  Tensor out = new RamTensor({ 1,12,12,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,2,2,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 4608; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_1_stride_2[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_2_stride_1) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<18432*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_2_stride_1);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_2_stride_1);
+  Tensor out = new RamTensor({ 1,24,24,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,1,1,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 18432; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_2_stride_1[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_2_stride_2) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<4608*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_2_stride_2);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_2_stride_2);
+  Tensor out = new RamTensor({ 1,12,12,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,2,2,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 4608; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_2_stride_2[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_3_stride_1) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<18432*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_3_stride_1);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_3_stride_1);
+  Tensor out = new RamTensor({ 1,24,24,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,1,1,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 18432; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_3_stride_1[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_3_stride_2) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<4608*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_3_stride_2);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_3_stride_2);
+  Tensor out = new RamTensor({ 1,12,12,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,2,2,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 4608; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_3_stride_2[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_4_stride_1) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<18432*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_4_stride_1);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_4_stride_1);
+  Tensor out = new RamTensor({ 1,24,24,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,1,1,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 18432; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_4_stride_1[i], 0.0001);
+  }
+}
+
+
+/*********************************************
+ * Generated Test number 
+ *********************************************/
+
+
+TEST(Convolution, random_inputs_VALID_4_stride_2) {
+  localCircularArenaAllocator<1024> meta_allocator;
+  localCircularArenaAllocator<4608*2*sizeof(float), uint32_t> ram_allocator;
+  Context::get_default_context()->set_metadata_allocator(&meta_allocator);
+  Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
+
+  Tensor in = new RomTensor({ 1,28,28,1 }, flt, s_in_VALID_4_stride_2);
+  Tensor w = new RomTensor({ 5,5,1,32 }, flt, s_w_VALID_4_stride_2);
+  Tensor out = new RamTensor({ 1,12,12,32 }, flt);
+
+  ConvOperator<float> conv_Aw({ 1,2,2,1}, VALID);
+  conv_Aw
+       .set_inputs({ {ConvOperator<float>::in, in}, {ConvOperator<float>::filter, w} })
+       .set_outputs({ {ConvOperator<float>::out, out} })
+       .eval();
+
+  for(int i = 0; i < 4608; i++) {
+    EXPECT_NEAR((float) out(i), s_ref_out_VALID_4_stride_2[i], 0.0001);
+  }
+}
