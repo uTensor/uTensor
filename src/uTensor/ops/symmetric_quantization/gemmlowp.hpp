c93bfc84e60a4ee4ba57c2001464fc9b69acc508
dboyliao <qmalliao@gmail.com>
2020-05-11 16:08:54 +0800

    Add gemmlowp module

---
+++ src/uTensor/ops/symmetric_quantization/gemmlowp.hpp
@@ -0,0 +1,17 @@
+#ifndef _UTENSOR_GEMMLOWP
+#define _UTENSOR_GEMMLOWP
+#include <cstdint>
+
+#include "errorHandler.hpp"
+
+namespace uTensor {
+namespace gemmlowp {
+
+DECLARE_ERROR(InvalidExponentError);
+
+std::int32_t SaturatingRoundingDoublingHighMul(std::int32_t a, std::int32_t b);
+std::int32_t RoundingDivideByPOT(std::int32_t x, std::int32_t exponent);
+
+}  // namespace gemmlowp
+}  // namespace uTensor
+#endif
