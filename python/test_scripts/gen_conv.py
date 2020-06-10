44f411d93206c54c5f8f99cb0c0a2e1857cc5f18
Michael Bartling <michael.bartling15@gmail.com>
2020-06-10 10:52:58 -0500

    Reorder conv weight params

--- python/test_scripts/gen_conv.py
+++ python/test_scripts/gen_conv.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import jinja2
 
-output_file = "test_convolution.cpp"
-const_file = "constants_convolution.hpp"
+output_file = "test_convolution_nobias.cpp"
+const_file = "constants_convolution_nobias.hpp"
 
 PADDING = "VALID"
 
@@ -40,12 +40,7 @@ container_str = """
 #include <cstring>
 #include <iostream>
 
-#include "Convolution.hpp"
-#include "BufferTensor.hpp"
-#include "RamTensor.hpp"
-#include "RomTensor.hpp"
-#include "arenaAllocator.hpp"
-#include "context.hpp"
+#include "uTensor.h"
 #include "gtest/gtest.h"
 
 #include "{{ constants_header }}"
@@ -53,6 +48,7 @@ using std::cout;
 using std::endl;
 
 using namespace uTensor;
+using uTensor::ReferenceOperators;
 
 {% for test in tests %}
 /*********************************************
@@ -76,25 +72,35 @@ const_Template = jinja2.Template(const_str)
 container_Template = jinja2.Template(container_str)
 const_container_Template = jinja2.Template(const_container_str)
 
-num_tests = 5
+num_tests = 1
 tests=[]
 constants=[]
-for i in range(num_tests):
-    for stride in [1, 2]:
-        w =  tf.Variable(tf.random.normal([5, 5, 1, 32]))
-        in_1 = tf.Variable(tf.random.normal([1, 28, 28, 1]))
-        strides = [1, stride, stride, 1]
-        out_1 = tf.nn.conv2d(in_1, w, strides=strides, padding=PADDING)
-        w_flat = w.numpy().flatten()
-        in_flat = in_1.numpy().flatten()
-        out_flat = out_1.numpy().flatten()
-        stride_str = "_stride_%d" % stride
-        test_name = "%s_%d%s" % (PADDING, i, stride_str)
+for padding in ["SAME", "VALID"]:
+    for i in range(num_tests):
+        for stride in [1, 2]:
+            w0 =  tf.Variable(tf.random.normal([5, 5, 1, 32]))
+            in_1 = tf.Variable(tf.random.normal([1, 28, 28, 3]))
+            strides = [1, stride, stride, 1]
+            out_1 = tf.nn.conv2d(in_1, w0, strides=strides, padding=padding)
+            # TF2 usess a different ordering of weights
+            w0_n = w0.numpy()
+            w = np.zeros((w0.shape[3], w0.shape[0], w0.shape[1], w0.shape[2]))
+            for i0 in range(w.shape[0]):
+                for i1 in range(w.shape[1]):
+                    for i2 in range(w.shape[2]):
+                        for i3 in range(w.shape[3]):
+                            w[i0, i1, i2, i3] = w0_n[i1, i2, i3, i0]
 
-        test_str_rendered = test_Template.render(test_name=test_name, input_size=in_flat.shape[0], w_size=w_flat.shape[0], out_size=out_flat.shape[0], ref_in=in_flat, ref_w=w_flat, ref_out=out_flat, strides=strides, in_shape=in_1.shape, w_shape=w.shape, out_shape=out_1.shape, PADDING=PADDING)
-        const_str_rendered = const_Template.render(test_name=test_name, input_size=in_flat.shape[0], w_size=w_flat.shape[0], out_size=out_flat.shape[0], ref_in=in_flat, ref_w=w_flat, ref_out=out_flat, strides=strides, in_shape=in_1.shape, w_shape=w.shape, out_shape=out_1.shape)
-        tests.append(test_str_rendered)
-        constants.append(const_str_rendered)
+            w_flat = w.flatten()
+            in_flat = in_1.numpy().flatten()
+            out_flat = out_1.numpy().flatten()
+            stride_str = "_stride_%d" % stride
+            test_name = "%s_%d%s" % (padding, i, stride_str)
+    
+            test_str_rendered = test_Template.render(test_name=test_name, input_size=in_flat.shape[0], w_size=w_flat.shape[0], out_size=out_flat.shape[0], ref_in=in_flat, ref_w=w_flat, ref_out=out_flat, strides=strides, in_shape=in_1.shape, w_shape=w.shape, out_shape=out_1.shape, PADDING=padding)
+            const_str_rendered = const_Template.render(test_name=test_name, input_size=in_flat.shape[0], w_size=w_flat.shape[0], out_size=out_flat.shape[0], ref_in=in_flat, ref_w=w_flat, ref_out=out_flat, strides=strides, in_shape=in_1.shape, w_shape=w.shape, out_shape=out_1.shape)
+            tests.append(test_str_rendered)
+            constants.append(const_str_rendered)
 
 container_rendered = container_Template.render(tests=tests, constants_header=const_file)
 consts_container_rendered = const_container_Template.render(constants=constants, constants_header=const_file)
