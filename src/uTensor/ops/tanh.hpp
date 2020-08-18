76fb5190477174bdf71719e3c3c6171411f03a06
dboyliao <qmalliao@gmail.com>
2020-08-18 17:06:59 +0800

    fix bug

--- src/uTensor/ops/tanh.hpp
+++ src/uTensor/ops/tanh.hpp
@@ -1,50 +1,50 @@
-#ifndef UTENSOR_MATRIX_OPS_H
-#define UTENSOR_MATRIX_OPS_H
+#ifndef UTENSOR_TANH_H
+#define UTENSOR_TANH_H
+#include <math.h>
+
 #include "context.hpp"
 #include "operatorBase.hpp"
-#include <math.h>
 // #include "ActivationFncs.hpp"  //in-plcae interface
 
 namespace uTensor {
 namespace ReferenceOperators {
 
 // adapt to tanh_kernel
-}
 
 template <typename OutputT, typename InputT>
 class TanhOperator : public OperatorInterface<1, 1> {
  public:
   enum names_in : uint8_t { act_in };
   enum names_out : uint8_t { act_out };
-  // AddOperator(FixedTensorMap<2> inputs, FixedTensorMap<1> outputs) :
-  // OperatorBase(inputs, outputs) {}
 
  protected:
-  virtual void compute() 
-    Tensor& in = inputs[act_in].tensor()
-    Tensor& out = inputs[act_out].tensor()
+  virtual void compute() {
+    Tensor& in = inputs[act_in].tensor();
+    Tensor& out = inputs[act_out].tensor();
 
     const int flat_size = in->get_shape().get_linear_size();
 
-    int32_t in_zero_point = in->get_quantization_params().get_zeroP_for_channel(0);
+    int32_t in_zero_point =
+        in->get_quantization_params().get_zeroP_for_channel(0);
     float in_scale = in->get_quantization_params().get_scale_for_channel(0);
 
-    int32_t out_zero_point = out->get_quantization_params().get_zeroP_for_channel(0);
+    int32_t out_zero_point =
+        out->get_quantization_params().get_zeroP_for_channel(0);
     float out_scale = out->get_quantization_params().get_scale_for_channel(0);
-
 
     for (int i = 0; i < flat_size; i++) {
       const int32_t in_val = static_cast<InputT>(in(i));
-      float float_in_val = static_cast<float>(in_scale * static_cast<float>((in_val - in_zero_point)));
+      float float_in_val = static_cast<float>(
+          in_scale * static_cast<float>((in_val - in_zero_point)));
       float element_activation = tanh(float_in_val);
-      OutputT result = static_cast<OutputT>(element_activation / out_scale + out_zero_point);
+      OutputT result =
+          static_cast<OutputT>(element_activation / out_scale + out_zero_point);
 
       out(i) = result;
     }
-
   }
 };
 
-}  // Reference Op
+}  // namespace ReferenceOperators
 }  // namespace uTensor
 #endif
