bfa4cb66d573d8ce958c4f12cc6c33b3f8fb6f12
dboyliao <qmalliao@gmail.com>
2020-11-20 17:22:58 +0800

    tranpose op: perm as input tensor

--- TESTS/operators/test_transpose.cpp
+++ TESTS/operators/test_transpose.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include "RamTensor.hpp"
-#include "Transpose.hpp"
 #include "RomTensor.hpp"
+#include "Transpose.hpp"
 #include "arenaAllocator.hpp"
 #include "constants_transpose.hpp"
 #include "context.hpp"
@@ -19,19 +19,19 @@ TEST(Transpose, transpose_test) {
   localCircularArenaAllocator<15 * 2 * sizeof(float), uint32_t> ram_allocator;
   Context::get_default_context()->set_metadata_allocator(&meta_allocator);
   Context::get_default_context()->set_ram_data_allocator(&ram_allocator);
-  
+
   Tensor input_tensor = new RomTensor({3, 1, 5, 1}, flt, random_input_arr);
+  Tensor perm_tensor = new RomTensor({4}, i32, transpose_perm_arr);
 
   TensorShape input_target_shape(3, 1, 5, 1);
   TensorShape input_shape = input_tensor->get_shape();
   EXPECT_TRUE(input_target_shape == input_shape);
 
-  Tensor transpose_axes = new RomTensor({4}, u8, transpose_axes_arr);
   Tensor output_tensor = new RamTensor(flt);
-  TransposeOperator<float> op({2,1,0,3});
+  TransposeOperator<float> op;
 
-
-  op.set_inputs({{TransposeOperator<float>::input, input_tensor}})
+  op.set_inputs({{TransposeOperator<float>::input, input_tensor},
+                 {TransposeOperator<float>::perm, perm_tensor}})
       .set_outputs({{TransposeOperator<float>::output, output_tensor}})
       .eval();
 
