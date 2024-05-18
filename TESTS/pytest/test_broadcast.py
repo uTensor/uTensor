c3f2cba9646f94f1d6980b1fd1ced761f1da79e1
yuanhenglee <yuanhenglee2000@gmail.com>
2024-05-18 15:25:31 +0800

    fix: remove redundant stuffs

--- TESTS/pytest/test_broadcast.py
+++ TESTS/pytest/test_broadcast.py
@@ -4,15 +4,8 @@ def test_broadcast_shapes(random_broadcastable_shapes: List[tuple]):
     import pyuTensor
     import numpy as np
 
-    pyuTensor.set_ram_total(100*1024)
-    pyuTensor.set_meta_total(100*1024)
-
     for shape_a, shape_b in random_broadcastable_shapes:
-        a = np.arange(np.prod(shape_a)).reshape(shape_a).astype(np.float32)
-        b = np.arange(np.prod(shape_b)).reshape(shape_b).astype(np.float32)
-
         bc = pyuTensor.Broadcaster(shape_a, shape_b)
         uT_1 = bc.get_shape_c()
         np_1 = np.broadcast_shapes(shape_a, shape_b)
-        assert isinstance(uT_1, tuple)
         assert uT_1 == np_1
