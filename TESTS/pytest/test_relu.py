bc8959b1e8fef65e00ed835d52633e5060b6280b
dboyliao <qmalliao@gmail.com>
2024-06-01 15:22:02 +0800

    update tests

--- TESTS/pytest/test_relu.py
+++ TESTS/pytest/test_relu.py
@@ -1,6 +1,7 @@
-import pytest
-import numpy as np
 from random import randint
+
+import numpy as np
+import pytest
 
 _NUM_TESTS = 10
 
@@ -11,8 +12,8 @@ def test_relu(arr):
     import pyuTensor
     import tensorflow as tf
 
-    pyuTensor.set_ram_total(4096)
-    pyuTensor.set_meta_total(4096)
+    pyuTensor.set_ram_total(1024*1000)
+    pyuTensor.set_meta_total(1024*1000)
     out = pyuTensor.relu(arr)
     tf_out = tf.nn.relu(arr).numpy()
     
